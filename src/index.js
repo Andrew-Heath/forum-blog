c6f1fdcfdc24d62856d018bf53360dc4fa6f4634
Andrew Heath <aheath42@gmail.com>
2017-06-27 20:37:39 -0700

    Fill dummy fies and update CSS for said dummy files

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import 'babel-polyfill';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import App from './app/containers/App';
+import configureStore from './app/store/configureStore';
 import {Router, Route, browserHistory} from 'react-router';
 
-import {Main} from './app/main';
-
 import './index.css';
+
+const store = configureStore();
 
 // Create the list of different front end routes
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}/>
-  </Router>,
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
