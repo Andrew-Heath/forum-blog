671d405a5643e810d8b627701563e49346a8058c
Andrew Heath <aheath42@gmail.com>
2017-07-05 15:14:01 -0700

    Organize and style header with name, email, and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends Component {
   render() {
     return (
       <footer className="footer">
-        this is the footer
+        Created by Andrew Heath | Email@: <a href="mailto:aheath42@gmail.com">aheath42@gmail.com</a> | <a href="http://www.github.com/andrew-heath">GitHub</a> | <a href="http://www.linkedin.com/in/andrew-heath">LinkedIn</a>
       </footer>
     );
   }
